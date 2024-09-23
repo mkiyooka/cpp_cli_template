de6feca0e9196fd56f600a2e4d3edcfbe1a3c6c9
Masahiro KIYOOKA <kiyookamasa@gmail.com>
2024-09-24 01:59:10 +0900

    change logger setup

--- include/setup_logger.hpp
+++ include/setup_logger.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
 #include <spdlog/spdlog.h>
@@ -8,37 +9,13 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/fmt/bundled/core.h>
 
-class SpdLogger {
-private:
+class ILogger {
+protected:
     std::shared_ptr<spdlog::logger> _logger;
 
 public:
-    explicit SpdLogger(const std::string &name) {
-        auto sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        _logger = std::make_shared<spdlog::logger>(name, sink);
-        _logger->set_pattern("[%^%L%$][%Y-%m-%d %T.%F][%t][%n] %v");
-        spdlog::register_logger(_logger);
-    }
-    SpdLogger(const std::string &name, const std::string &pattern) {
-        auto sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        _logger = std::make_shared<spdlog::logger>(name, sink);
-        _logger->set_pattern(pattern);
-        spdlog::register_logger(_logger);
-    }
-    ~SpdLogger() = default;
-    SpdLogger(const SpdLogger &) = delete;
-    SpdLogger &operator=(SpdLogger const &) = delete;
-    SpdLogger(SpdLogger &&) = delete;
-    SpdLogger &operator=(SpdLogger &&) = delete;
-    void setPatternDefault() { _logger->set_pattern("[%Y-%m-%d %T.%e][%^%L%$][%n] %v"); }
+    // clang-format off
     void setPattern(const std::string &pattern) { _logger->set_pattern(pattern); }
-
-    void setLevelTrace() { _logger->set_level(spdlog::level::trace); }
-    void setLevelDebug() { _logger->set_level(spdlog::level::debug); }
-    void setLevelInfo() { _logger->set_level(spdlog::level::info); }
-    void setLevelWarn() { _logger->set_level(spdlog::level::warn); }
-    void setLevelError() { _logger->set_level(spdlog::level::err); }
-    void setLevelCritical() { _logger->set_level(spdlog::level::critical); }
     void setLevel(const std::string &logLevel) {
         if (logLevel == "trace") {
             _logger->set_level(spdlog::level::trace);
@@ -54,11 +31,53 @@ public:
             _logger->set_level(spdlog::level::critical);
         }
     }
-    void trace(const std::string &s) const { _logger->trace(s); }
-    void debug(const std::string &s) const { _logger->debug(s); }
-    void info(const std::string &s) const { _logger->info(s); }
-    void warn(const std::string &s) const { _logger->warn(s); }
-    void error(const std::string &s) const { _logger->error(s); }
-    void critical(const std::string &s) const { _logger->critical(s); }
+    void setLevelTrace()    { _logger->set_level(spdlog::level::trace);    }
+    void setLevelDebug()    { _logger->set_level(spdlog::level::debug);    }
+    void setLevelInfo()     { _logger->set_level(spdlog::level::info);     }
+    void setLevelWarn()     { _logger->set_level(spdlog::level::warn);     }
+    void setLevelError()    { _logger->set_level(spdlog::level::err);      }
+    void setLevelCritical() { _logger->set_level(spdlog::level::critical); }
+
+    inline void trace    (const std::string &s) const { _logger->trace(s);    }
+    inline void debug    (const std::string &s) const { _logger->debug(s);    }
+    inline void info     (const std::string &s) const { _logger->info(s);     }
+    inline void warn     (const std::string &s) const { _logger->warn(s);     }
+    inline void error    (const std::string &s) const { _logger->error(s);    }
+    inline void critical (const std::string &s) const { _logger->critical(s); }
+    // clang-format on
     std::shared_ptr<spdlog::logger> get() const { return _logger; }
 };
+
+class ConsoleLogger : public ILogger {
+public:
+    explicit ConsoleLogger(const std::string &name, const std::string &pattern = "[%L][%Y-%m-%d %T.%f][%n] %v") {
+        auto sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        _logger = std::make_shared<spdlog::logger>(name, sink);
+        _logger->set_pattern(pattern);
+        spdlog::register_logger(_logger);
+    }
+    ConsoleLogger(const ConsoleLogger &) = delete;            // copy constructor
+    ConsoleLogger(ConsoleLogger &&) = delete;                 // move constructor
+    ConsoleLogger &operator=(ConsoleLogger const &) = delete; // copy
+    ConsoleLogger &operator=(ConsoleLogger &&) = delete;      // move
+    ~ConsoleLogger() = default;
+};
+
+class FileLogger : public ILogger {
+public:
+    explicit FileLogger(
+        const std::string &name, const std::string &filename = "./log/basic.log",
+        const std::string &pattern = "[%L][%Y-%m-%d %T.%f][%n] %v") {
+        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_st>(filename);
+        _logger = std::make_shared<spdlog::logger>(name, sink);
+        spdlog::initialize_logger(_logger);
+        sink->set_pattern("[%L][%Y-%m-%d %T.%f][%n] %v");
+        // spdlog::register_logger(_logger);
+    }
+    FileLogger(const FileLogger &) = delete;            // copy constructor
+    FileLogger(FileLogger &&) = delete;                 // move constructor
+    FileLogger &operator=(FileLogger const &) = delete; // copy
+    FileLogger &operator=(FileLogger &&) = delete;      // move
+    ~FileLogger() = default;
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
+#include <memory>
 #include <string>
-
-#include "cli.hpp"
-#include "spdlog/common.h"
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
@@ -10,82 +8,30 @@
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/fmt/bundled/core.h>
 
-class Logger {
-private:
-    // std::shared_ptr<spdlog::sinks::basic_file_sink_st> _sink;
-    // std::shared_ptr<spdlog::logger> _loggerA;
-    // std::shared_ptr<spdlog::logger> _loggerB;
-    std::vector<std::shared_ptr<spdlog::logger>> _loggers;
+#include "cli.hpp"
+#include "setup_logger.hpp"
 
-public:
-    explicit Logger() {
-        auto _sink = std::make_shared<spdlog::sinks::basic_file_sink_st>("./logs/basicAB.log");
-        auto _loggerA = std::make_shared<spdlog::logger>("loggerA", _sink);
-        spdlog::initialize_logger(_loggerA);
-        auto _loggerB = std::make_shared<spdlog::logger>("loggerB", _sink);
-        spdlog::initialize_logger(_loggerB);
-        // _sink->set_pattern("[%L][%E.%f][%n] %v");
-        _sink->set_pattern("[%L][%Y-%m-%d %T.%f][%n] %v");
-        // _sink->set_pattern("[%Y-%m-%d %T.%f][%n] %v");
+void func1(const std::shared_ptr<ILogger> &logger) {
+    logger->info("this is log@func1");
+}
 
-        _loggerA->set_level(spdlog::level::debug);
-        _loggerB->set_level(spdlog::level::info);
-        // _loggers.push_back(_loggerA);
-        // _loggers.push_back(_loggerB);
-    }
-    Logger &operator=(Logger const &) = delete;
-    Logger(Logger &&) = delete;
-    Logger &operator=(Logger &&) = delete;
-    ~Logger() = default;
-};
-
-void func1() {
-    auto logger1 = spdlog::get("logger1");
-    logger1->debug("logger1@func1 debug");
-    logger1->info("logger1@func1 info");
-    auto logger2 = spdlog::get("logger2");
-    logger2->debug("logger2@func1 debug");
-    logger2->info("logger2@func1 info");
-
-    auto loggerA = spdlog::get("loggerA");
-    loggerA->debug("loggerA@func1 debug");
-    loggerA->info("loggerA@func1 info");
-    auto loggerB = spdlog::get("loggerB");
-    loggerB->debug("loggerB@func1 debug");
-    loggerB->info("loggerB@func1 info");
+void func2(const std::shared_ptr<ILogger> &logger) {
+    logger->info("this is log@func2");
 }
 
 int main(int argc, char *argv[]) {
+    cli(argc, argv); // Parse arguments for CLI
 
-    auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("./logs/basic12.log");
-    auto logger1 = std::make_shared<spdlog::logger>("logger1", sink);
-    spdlog::initialize_logger(logger1);
-    auto logger2 = std::make_shared<spdlog::logger>("logger2", sink);
-    spdlog::initialize_logger(logger2);
-    // sink->set_pattern("[%^%L%$][%E.%f][%n] %v"); // epoch time
-    sink->set_pattern("[%^%L%$][%Y-%m-%d %T.%f][%n] %v");
-    logger1->set_level(spdlog::level::debug);
-
-    // auto logger1 = spdlog::get("logger1");
-    // auto logger2 = spdlog::get("logger2");
-    logger1->info("logger1@main info");
-    logger2->info("logger2@main info");
-    logger1->debug("logger1@main debug");
-    logger2->debug("logger2@main debug");
-
-    Logger logger;
-
-    auto loggerA = spdlog::get("loggerA");
-    auto loggerB = spdlog::get("loggerB");
-    loggerA->info("loggerA@main info");
-    loggerB->info("loggerB@main info");
-    loggerA->debug("loggerA@main debug");
-    loggerB->debug("loggerB@main debug");
-
-    func1();
-
-    // Parse arguments for CLI
-    cli(argc, argv);
+    std::shared_ptr<ILogger> logger = nullptr;
+    const std::string s = "file";
+    if (s == "file") {
+        logger = std::make_shared<FileLogger>("Logger2", "./log/basic.log");
+    } else {
+        logger = std::make_shared<ConsoleLogger>("Logger1");
+    }
+    logger->info("this is log@main");
+    func1(logger);
+    func2(logger);
 
     return 0;
 }
